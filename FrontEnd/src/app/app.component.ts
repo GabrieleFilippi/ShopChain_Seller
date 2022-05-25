273b82953e58783969e61d575ac8f0d7653ef1fe
GabrieleFilippi <gabri.fil99@gmail.com>
2022-05-25 19:57:42 +0200

    Registrazione Seller1&2

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -15,11 +15,16 @@ export class AppComponent {
   public userTotalToken: any;
   public signerAddress: any;
   public balance: any;
+  public connectWallet: any;
+  public sellers: [] = [];
   async ngOnInit(){
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     this.signer = provider.getSigner(0);
     this.tokenContract = new ethers.Contract(address.contractAddress, address.abi, this.signer);
     this.balance = await this.tokenContract.getBalance();
     console.log(this.balance);
+    this.sellers = await this.tokenContract.getSellers();
+    console.log(this.sellers);
+    //this.tokenContract.registerAsSeller();
+    }
   }
-}
