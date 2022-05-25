3a32e809dd50ce99fc477c5a84b66dd560fd498e
GabrieleFilippi <gabri.fil99@gmail.com>
2022-05-25 17:42:02 +0200

    Deploy

---
+++ ContractDeploy/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ const ALCHEMY_API_KEY = "wCabfKLw6ozClq__UJ4t9xFmLwTtH4yF";
+
+ // Replace this private key with your Ropsten account private key
+ // To export your private key from Metamask, open Metamask and
+ // go to Account Details > Export Private Key
+ // Be aware of NEVER putting real Ether into testing accounts
+ const FUJI_PRIVATE_KEY = "ab24df078e882ccb8c0b7dbbaf220daf40ea9dc64756a5c894d4647a9dc0d68f";
+ const FUJI_PUBLIC_KEY = "0xFDf572Ce0b87f7036f9F528FD55968348Bd86081"
+module.exports = {
+  solidity: "0.8.0",
+  networks:{
+    fuji:{
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      accounts: [`${FUJI_PRIVATE_KEY}`]
+    }
+  }
+};
---
+++ ContractDeploy/test/ShopChain.js
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+
+describe("Token contract", function () {
+    it()
+    const owner = await ethers.getSigners();
+});
