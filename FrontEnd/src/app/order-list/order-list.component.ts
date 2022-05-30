d745531d8d4c450d535d98389de571e3b35229a6
GabrieleFilippi <gabri.fil99@gmail.com>
2022-05-30 16:02:07 +0200

    removed orderclass

--- FrontEnd/src/app/order-list/order-list.component.ts
+++ FrontEnd/src/app/order-list/order-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { listenerCount } from 'process';
 import { MetamaskConnectionService } from '../metamask-connection.service';
 import { Orders, State } from '../orders';
-import { Order } from '../orderclass';
 import { ethers} from 'ethers';
 //import * as initOrders from '../orders';
 @Component({
@@ -11,7 +10,6 @@ import { ethers} from 'ethers';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
-  order: Order | undefined;
   userOrdersList: Orders[] = [];
   orderList: any;
   sellerList: any;
@@ -35,20 +33,9 @@ export class OrderListComponent implements OnInit {
   async getUserOrders(){
     const userAddress = await this.getUser();
     this.userOrders = await this.metamaskConnectionService.getUserOrderList(userAddress);
-    //this.userOrdersList = await this.formatOrder(this.userOrders);
     this.userOrdersList = await this.formatOrderI(this.userOrders);
     return this.userOrders;
   }
-  // funziona con orderclass ma meglio usare un interfaccia
-  async formatOrder(list: any[]){
-    const LIST: Order[] = [];
-      for (let i = 0; i < list.length; i ++) {
-        const chunk = list.slice(i, i+1);
-        const order = new Order(chunk[0][0],chunk[0][1],chunk[0][2],chunk[0][3],chunk[0][4]);
-        LIST!.push(order);
-    }
-    return LIST;
-  }  
   // funziona con l'interfaccia
   async formatOrderI(list: any[]){
     const LIST: Orders[] = []
